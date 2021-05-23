e58e33027fe805317cb92081e7ac70d3ae6f26cc
Radu <radu.stefan@dynatrace.com>
2021-05-23 21:24:41 +0100

    added license

--- cmd/main.go
+++ cmd/main.go
@@ -1,3 +1,25 @@
+/**
+ * Copyright (c) 2021 Radu Stefan
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ **/
+
 package main
 
 import (
@@ -7,82 +29,3 @@ import (
 func main() {
 	cli.NewCli()
 }
-
-// func buildCli() *cli.App {
-// 	app := cli.NewApp()
-
-// 	app.Usage = "Simulates fleets of taxies operating and pushing metrics to Dynatrace via the API."
-// 	app.Version = version
-
-// 	cli.VersionPrinter = func(c *cli.Context) {
-// 		fmt.Println(c.App.Version)
-// 	}
-// 	cli.VersionFlag = &cli.BoolFlag{
-// 		Name:  "version",
-// 		Usage: "print the version and exit",
-// 	}
-
-// 	app.Description = `
-// 	Tool used to simulate generic entities, their relationships, and metrics. The scenario simulates a
-// 	fleet of smart taxies which generate metrics that are then pushed out to a Dynatrace tenant via API.
-// 	`
-// 	startCommand := getStartCommand()
-// 	app.Commands = []*cli.Command{&startCommand}
-
-// 	return app
-// }
-
-// func getStartCommand() cli.Command {
-// 	command := cli.Command{
-// 		Name:      "start",
-// 		Usage:     "starts the simulation",
-// 		UsageText: "start [command options]",
-// 		Before: func(c *cli.Context) error {
-// 			fmt.Println("Fleet Simulator v" + version)
-// 			return nil
-// 		},
-// 		Flags: []cli.Flag{
-// 			&cli.StringFlag{
-// 				Name:     "environment",
-// 				Aliases:  []string{"e"},
-// 				Usage:    "Dynatrace SaaS or Managed tenant and domain. You don't need https:// or the ending slash.",
-// 				Required: true,
-// 			},
-// 			&cli.StringFlag{
-// 				Name:     "token",
-// 				Aliases:  []string{"t"},
-// 				Usage:    "Dynatrace API Token with Metrics (V2) permission",
-// 				Required: true,
-// 			},
-// 			&cli.IntFlag{
-// 				Name:    "fleets",
-// 				Aliases: []string{"f"},
-// 				Usage:   "Number of fleets to simulate (max. 10). Will impact total number of taxis and therefore metrics ingested.",
-// 				Value:   2,
-// 			},
-// 			&cli.StringFlag{
-// 				Name:    "taxisPerFleet",
-// 				Aliases: []string{"tpf"},
-// 				Usage:   "Number of taxis per fleet to simulate. Can give a range using the 'min-max' format for more variety.",
-// 				Value:   "5",
-// 			},
-// 		},
-// 		Action: func(ctx *cli.Context) error {
-// 			if ctx.Args().Present() {
-// 				fmt.Printf("Error:\n\tFound argument: %s\n\tThis command uses only flags, no arguments are needed.\n", ctx.Args().First())
-// 				cli.ShowAppHelpAndExit(ctx, 1)
-// 			}
-
-// 			env := parseFlagEnvironment(ctx.String("environment"))
-// 			fleets := parseFlagNumFleets(ctx.Int("fleets"))
-// 			client := newDTClient(env, ctx.String("token"))
-
-// 			return startSimulation(
-// 				client,
-// 				fleets,
-// 				ctx.String("taxisPerFleet"),
-// 			)
-// 		},
-// 	}
-// 	return command
-// }
